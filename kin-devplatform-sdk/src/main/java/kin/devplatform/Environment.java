825c19ddc81a1e98def2291997b1facc21dddc53
Amitai Efrati <amitai.efrati@gmail.com>
2018-09-17 14:51:57 +0300

    Update environment production urls

--- kin-devplatform-sdk/src/main/java/kin/devplatform/Environment.java
+++ kin-devplatform-sdk/src/main/java/kin/devplatform/Environment.java
@@ -2,13 +2,8 @@ package kin.devplatform;
 
 public class Environment implements KinEnvironment {
 
-	private static final Environment PLAYGROUND = new Environment(
-		"https://horizon-playground.kininfrastructure.com/",
-		"Kin Playground Network ; June 2018",
-		"GBC3SG6NGTSZ2OMH3FFGB7UVRQWILW367U4GSOOF4TFSZONV42UJXUH7",
-		"https://api.developers.kinecosystem.com/v1",
-		"https://s3.amazonaws.com/assets.kinplayground.com/web-offers/cards-based/index.html",
-		"https://kin-bi.appspot.com/devp_play_");
+	private static Environment PLAYGROUND;
+	private static Environment PRODUCTION;
 
 	private final String blockchainNetworkUrl;
 	private final String blockchainPassphrase;
@@ -58,6 +53,29 @@ public class Environment implements KinEnvironment {
 	}
 
 	public static KinEnvironment getPlayground() {
+		if (PLAYGROUND == null) {
+			PLAYGROUND = new Environment(
+				"https://horizon-playground.kininfrastructure.com/",
+				"Kin Playground Network ; June 2018",
+				"GBC3SG6NGTSZ2OMH3FFGB7UVRQWILW367U4GSOOF4TFSZONV42UJXUH7",
+				"https://api.developers.kinecosystem.com/v1",
+				"https://s3.amazonaws.com/assets.kinplayground.com/web-offers/cards-based/index.html",
+				"https://kin-bi.appspot.com/devp_play_");
+		}
 		return PLAYGROUND;
 	}
+
+	public static KinEnvironment getProduction() {
+		if (PRODUCTION == null) {
+			PRODUCTION = new Environment(
+				"https://horizon-ecosystem.kininfrastructure.com/",
+				"Public Global Kin Ecosystem Network ; June 2018",
+				"GDF42M3IPERQCBLWFEZKQRK77JQ65SCKTU3CW36HZVCX7XX5A5QXZIVK",
+				"https://api-prod.developers.kinecosystem.com/v1",
+				"https://s3.amazonaws.com/assets.developers.kinecosystem.com/web-offers/cards-based/index.html",
+					"https://kin-bi.appspot.com/devp_");
+		}
+		return PRODUCTION;
+	}
+
 }
