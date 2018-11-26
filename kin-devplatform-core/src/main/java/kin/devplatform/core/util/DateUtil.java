06c6282d1c4440ce2a478fbdf2c1057859d134d2
Amitai Efrati <amitai.efrati@gmail.com>
2018-11-26 09:16:57 +0200

    Fix bug number DP-98

--- kin-devplatform-core/src/main/java/kin/devplatform/core/util/DateUtil.java
+++ kin-devplatform-core/src/main/java/kin/devplatform/core/util/DateUtil.java
@@ -9,14 +9,14 @@ import java.util.TimeZone;
 
 public class DateUtil {
 
-	private static DateFormat dateFormat = new SimpleDateFormat("dd.MM.yy", Locale.US);
 	private static DateFormat utcDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm", Locale.US);
+	private static DateFormat localFormat = DateFormat.getDateInstance(DateFormat.SHORT ,Locale.getDefault());
 	private static TimeZone utcTimeZone = TimeZone.getTimeZone("UTC");
 
 	public static String getDateFormatted(String dateStr) {
-		Date date = getDateFromUTCString(dateStr);
+;		Date date = getDateFromUTCString(dateStr);
 		if (date != null) {
-			return dateFormat.format(date);
+			return localFormat.format(getDateFromUTCString(dateStr));
 		} else {
 			return "";
 		}
