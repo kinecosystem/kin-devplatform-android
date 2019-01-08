aedb2204d7b01070701c8fba0f9dd3326f588fc4
Yossi Segev <yos.segev@gmail.com>
2019-01-08 16:31:13 +0200

    Implement migration event listener in version provider

--- kin-devplatform-sdk/src/main/java/kin/devplatform/network/model/KinVersionProvider.java
+++ kin-devplatform-sdk/src/main/java/kin/devplatform/network/model/KinVersionProvider.java
@@ -1,22 +1,34 @@
 package kin.devplatform.network.model;
 
+import android.support.annotation.NonNull;
+
 import kin.devplatform.core.network.ApiException;
 import kin.devplatform.network.api.KinVersionApi;
+import kin.sdk.migration.bi.IMigrationEventsListener;
 import kin.sdk.migration.exception.FailedToResolveSdkVersionException;
 import kin.sdk.migration.interfaces.IKinVersionProvider;
 
 public class KinVersionProvider implements IKinVersionProvider {
+
+    private IMigrationEventsListener migrationEventsListener;
+
+    public KinVersionProvider(@NonNull IMigrationEventsListener migrationEventsListener) {
+        this.migrationEventsListener = migrationEventsListener;
+    }
 
     @Override
     public SdkVersion getKinSdkVersion(String appId) throws FailedToResolveSdkVersionException {
         KinVersionApi kinVersionApi = new KinVersionApi();
         SdkVersion sdkVersion;
         try {
+            migrationEventsListener.onVersionCheckStart();
             String blockchainVersion = kinVersionApi.getBlockchainVersion(appId);
             sdkVersion = SdkVersion.get(blockchainVersion);
         } catch (ApiException e) {
+            migrationEventsListener.onVersionCheckFailed(e);
             throw new FailedToResolveSdkVersionException(e);
         }
+        migrationEventsListener.onVesrsionReceived(sdkVersion);
         return sdkVersion;
     }
 }
